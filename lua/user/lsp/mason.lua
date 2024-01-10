e415f04fe12116aded614f08ed3db06a15c5e4d7
menurhosain <omithasan005@gmail.com>
2024-01-10 19:54:12 +0600

    add: lsp

---
+++ lua/user/lsp/mason.lua
@@ -0,0 +1,46 @@
+local servers = {
+	"lua_ls",
+	"cssls",
+	"html",
+	"tsserver",
+	"dockerls",
+	"docker_compose_language_service",
+	"intelephense",
+	"solidity",
+	"rust_analyzer"
+}
+
+local settings = {
+	ui = {
+		border = "none",
+		icons = {
+			package_installed = "◍",
+			package_pending = "◍",
+			package_uninstalled = "◍",
+		},
+	},
+	log_level = vim.log.levels.INFO,
+	max_concurrent_installers = 4,
+}
+
+require("mason").setup(settings)
+require("mason-lspconfig").setup({
+	ensure_installed = servers,
+	automatic_installation = true,
+})
+
+local lspconfig_status_ok, lspconfig = pcall(require, "lspconfig")
+if not lspconfig_status_ok then
+	return
+end
+
+local opts = {}
+
+for _, server in pairs(servers) do
+	opts = {
+		on_attach = require("user.lsp.handlers").on_attach,
+		capabilities = require("user.lsp.handlers").capabilities,
+	}
+
+	lspconfig[server].setup(opts)
+end
--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -52,10 +52,18 @@ return packer.startup(function(use)
   use "hrsh7th/cmp-path" -- path completions
   use "hrsh7th/cmp-cmdline" -- cmdline completions
   use "saadparwaiz1/cmp_luasnip" -- snippet completions
+  use { "hrsh7th/cmp-nvim-lsp" } -- lsp completion
+  use { "hrsh7th/cmp-nvim-lua" } -- nvim-cmp source for neovim Lua API.
 
   -- snippets
   use "L3MON4D3/LuaSnip" --snippet engine
   use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
+
+  -- LSP
+  use { "neovim/nvim-lspconfig" } -- enable LSP
+  use { "williamboman/mason.nvim" } -- simple to use language server installer
+  use { "williamboman/mason-lspconfig.nvim" } -- mason extention
+  use { "folke/trouble.nvim", "nvim-tree/nvim-web-devicons" } --for diagnostics
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
