3b98c72f05d9fb9e8bd016e5f7dc2f17347d4572
menurhosain <omithasan005@gmail.com>
2024-05-04 11:09:11 +0600

    new json language server added and disable formating for solidity

--- lua/user/formatter.lua
+++ lua/user/formatter.lua
@@ -20,14 +20,43 @@ conform.setup({
     scss = { { "prettierd", "prettier" } },
     less = { { "prettierd", "prettier" } },
   },
+
   -- If this is set, Conform will run the formatter on save.
   -- It will pass the table to conform.format().
   -- This can also be a function that returns the table.
-  format_on_save = {
-    -- I recommend these options. See :help conform.format for details.
-    lsp_fallback = true,
-    timeout_ms = 500,
-  },
+  format_on_save = function(bufnr)
+    -- Disable autoformat on certain filetypes
+    local ignore_filetypes = { "sol" }
+    if vim.tbl_contains(ignore_filetypes, vim.bo[bufnr].filetype) then
+      return
+    end
+    -- Disable with a global or buffer-local variable
+    if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+      return
+    end
+    -- ...additional logic...
+    return { timeout_ms = 500, lsp_fallback = true }
+  end,
+
+  vim.api.nvim_create_user_command("FormatDisable", function(args)
+    if args.bang then
+      -- FormatDisable! will disable formatting just for this buffer
+      vim.b.disable_autoformat = true
+    else
+      vim.g.disable_autoformat = true
+    end
+  end, {
+    desc = "Disable autoformat-on-save",
+    bang = true,
+  }),
+
+  vim.api.nvim_create_user_command("FormatEnable", function()
+    vim.b.disable_autoformat = false
+    vim.g.disable_autoformat = false
+  end, {
+    desc = "Re-enable autoformat-on-save",
+  }),
+
   -- If this is set, Conform will run the formatter asynchronously after save.
   -- It will pass the table to conform.format().
   -- This can also be a function that returns the table.
@@ -39,4 +68,3 @@ conform.setup({
   -- Conform will notify you when a formatter errors
   notify_on_error = true,
 })
-
--- lua/user/lsp/mason.lua
+++ lua/user/lsp/mason.lua
@@ -1,51 +1,54 @@
 local servers = {
-	"lua_ls",
-	"cssls",
-	"html",
-	"emmet_ls",
-	"tsserver",
-	"dockerls",
-	"docker_compose_language_service",
-	"intelephense",
-	"solidity",
-	"rust_analyzer"
+  "lua_ls",
+  "cssls",
+  "html",
+  "emmet_ls",
+  "tsserver",
+  "dockerls",
+  "docker_compose_language_service",
+  "intelephense",
+  "jsonls",
+  -- "solidity",
+  -- "solc",
+  "rust_analyzer"
 }
 
 local settings = {
-	ui = {
-		border = "none",
-		icons = {
-			package_installed = "◍",
-			package_pending = "◍",
-			package_uninstalled = "◍",
-		},
-	},
-	log_level = vim.log.levels.INFO,
-	max_concurrent_installers = 4,
+  ui = {
+    border = "none",
+    icons = {
+      package_installed = "◍",
+      package_pending = "◍",
+      package_uninstalled = "◍",
+    },
+  },
+  log_level = vim.log.levels.INFO,
+  max_concurrent_installers = 4,
 }
 
 require("mason").setup(settings)
 require("mason-lspconfig").setup({
-	ensure_installed = servers,
-	automatic_installation = true,
+  ensure_installed = servers,
+  automatic_installation = true,
 })
 
 local lspconfig_status_ok, lspconfig = pcall(require, "lspconfig")
 if not lspconfig_status_ok then
-	return
+  return
 end
 
 local opts = {}
 
 for _, server in pairs(servers) do
-	opts = {
-		on_attach = require("user.lsp.handlers").on_attach,
-		capabilities = require("user.lsp.handlers").capabilities,
-	}
+  opts = {
+    on_attach = require("user.lsp.handlers").on_attach,
+    capabilities = require("user.lsp.handlers").capabilities,
+  }
 
-	if server == "emmet_ls" then
-    opts.filetypes = { "css", "eruby", "html", "javascript", "javascriptreact", "less", "sass", "scss", "svelte", "pug", "typescriptreact", "vue" }
-	end
+  if server == "emmet_ls" then
+    opts.filetypes = { "css", "eruby", "html", "javascript", "javascriptreact", "less", "sass", "scss", "svelte", "pug",
+      "typescriptreact", "vue" }
+  end
 
-	lspconfig[server].setup(opts)
+  lspconfig[server].setup(opts)
 end
